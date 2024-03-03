22e892181a9625a93b637d0026d7ac2be1f55ee2
Ashwin7p <ashwin.mpatil98@gmail.com>
2024-03-02 20:36:16 -0700

    Auth Token implementation init comit

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    'app'
+    'app',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'user'
 ]
 
 MIDDLEWARE = [
